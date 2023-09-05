c00966469324e42d9855cb7ae1e1421b37a39bbb
Soburr <adebesinbenzenejr99@gmail.com>
2023-09-05 03:04:02 +0100

    feature/added-alpine,js-to-followUsers

--- resources/views/livewire/follow-user.blade.php
+++ resources/views/livewire/follow-user.blade.php
@@ -1,16 +1,10 @@
-<div>
-    <div x-data="{ isFollowing: @json($user->isFollowedBy(auth()->user())) }">
-        <button
-            x-show="!isFollowing"
-            @click="followUser"
-        >
-            Follow
-        </button>
-        <button
-            x-show="isFollowing"
-            @click="unfollowUser"
-        >
-            Unfollow
-        </button>
-    </div>
+<style>
+    [x-cloak] {
+        display: none;
+    }
+</style>
+
+<div x-data="{ isFollowing: true}">
+    <button class="btn btn-success" x-on:click="isFollowing = true" x-show="!isFollowing" x-cloak>Follow</button>
+    <button class="btn btn-primary" x-on:click="isFollowing = !isFollowing" x-show="isFollowing" x-cloak>Unfollow</button>
 </div>
